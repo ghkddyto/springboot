b59ab869ac377438363110390c774382cd4135b8
yshwang <yshwang@sphereax.com>
2024-09-20 16:30:52 +0900

    spring start

---
+++ src/main/java/me/yosae/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package me.yosae.repository;
+
+import me.yosae.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
