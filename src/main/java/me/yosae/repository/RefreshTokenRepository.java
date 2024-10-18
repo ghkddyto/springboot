ac04148704e2bdbdbda37a104f29e393515a2496
yshwang <yshwang@sphereax.com>
2024-10-18 17:59:10 +0900

    09장 jwt 토큰 구현 1

---
+++ src/main/java/me/yosae/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package me.yosae.repository;
+
+import me.yosae.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+}
